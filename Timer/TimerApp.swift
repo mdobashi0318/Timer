ecb32e639c9d34b0d18ab2337c00166a495bc52d
MasaharuDobashi <masaharu.dobashi@gmail.com>
2022-12-19 22:18:01 +0900

    Initial Commit

---
+++ Timer/TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerApp.swift
+//  Timer
+//
+//  Created by 土橋正晴 on 2022/12/19.
+//
+
+import SwiftUI
+
+@main
+struct TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
